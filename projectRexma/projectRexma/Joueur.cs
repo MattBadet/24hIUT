75ac18e10c988baaae5ffa66ae04c70690771598
Florian Marteau <Florian_Marteau@etu.u-bourgogne.fr>
2022-04-02 13:44:20 +0200

    modif joueur

--- projectRexma/projectRexma/Joueur.cs
+++ projectRexma/projectRexma/Joueur.cs
@@ -6,11 +6,41 @@ namespace projectRexma
 {
     public class Joueur
     {
+
+        #region Attributs
         // Camp du joueur
         private string camp;
 
         // Ordre du joueur par rapport aux autres
         private int numeroOrdre;
 
+
+        // Main du joueur
+        private Main main;
+
+        #endregion
+
+
+        #region Methodes
+        // Création d'un joueur
+        public Joueur(String camp)
+        {
+            this.camp = camp;
+            this.main = new Main();
+        }
+
+
+        // Piocher une carte
+        public void Piocher(string numeroCarte)
+        {
+            // Piocher la carte demandée
+            Console.WriteLine($"PIOCHER|{numeroCarte}");
+            // Réagir à la réponse du serveur
+
+            //Ajouter la carte dans la liste
+            //this.main.AjouterCarte()
+        }
+
+        #endregion
     }
 }
