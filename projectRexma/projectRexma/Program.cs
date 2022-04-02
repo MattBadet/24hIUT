0c245c25ef2400175f9f760faa78e424da78ca82
FutBoy21 <leo.fontaine@iut-dijon.u-bourgogne.fr>
2022-04-02 12:05:39 +0200

    Tets Ajout

--- projectRexma/projectRexma/Program.cs
+++ projectRexma/projectRexma/Program.cs
@@ -8,6 +8,7 @@ namespace projectRexma
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Flo le bg");
+            Console.WriteLine("Léo Aussi")
         }
     }
 }
