a9554b57b5769c6290609a0f0e968f8b77bd56cf
Florian Marteau <Florian_Marteau@etu.u-bourgogne.fr>
2022-04-02 12:42:33 +0200

    attribut/propri

--- projectRexma/projectRexma/Carte.cs
+++ projectRexma/projectRexma/Carte.cs
@@ -6,5 +6,39 @@ namespace projectRexma
 {
     public class Carte
     {
+        // Type de la carte
+        private TypeCarte typeCarte;
+
+        // Renvoie le nombre de points de la carte
+        public double Point
+        {
+            get {
+                double Result = 0;
+                switch (this.typeCarte)
+                {
+                    case TypeCarte.ALIGOTE:
+                        Result = 0.8;
+                        break;
+                    case TypeCarte.CHARDONNAY:
+                        Result = 1.0;
+                        break;
+                    case TypeCarte.GAMAY:
+                        Result = 0.9;
+                        break;
+                    case TypeCarte.PINOY:
+                        Result = 1.2;
+                        break;
+                    
+                }
+                return Result;
+            }
+        }
+
+
+
+
+
+
+
     }
 }
