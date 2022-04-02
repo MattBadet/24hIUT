ed76b5dd7afb56c1f5f08208ed26651dc208a032
Florian Marteau <Florian_Marteau@etu.u-bourgogne.fr>
2022-04-02 12:27:07 +0200

    ajout/modif typeCarte

---
+++ projectRexma/projectRexma/TypeCarte.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projectRexma
+{
+    enum TypeCarte
+    {
+        ALIGOTE,
+        CHARDONNAY,
+        GAMAY,
+        PINOY
+    }
+}
