720736af9d2d8c5a5dab0775397628d51b5ee693
FutBoy21 <leo.fontaine@iut-dijon.u-bourgogne.fr>
2022-04-02 11:34:53 +0200

    Création du projet

---
+++ projectRexma/projectRexma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projectRexma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
